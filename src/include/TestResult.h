7f70a3b713c8de901efb214d1ab1f6e44a2cde45
DefenderJohn <4aaaapus@gmail.com>
2024-09-18 14:06:25 +0800

    完成少量增改

--- src/include/TestResult.h
+++ src/include/TestResult.h
@@ -1,5 +1,18 @@
+#include <vector>
+#include <string>
 class TestResult {
 public:
     bool success;
-    void appendSubTestResult(TestResult &subTestRes){}
+    bool isLeaf;
+    u_int32_t currentTestIndex;
+    u_int32_t totalSubTests;
+    std::vector<u_int32_t> failedSubTests;
+    std::vector<std::string> testOutput;
+    void appendSubTestResult(TestResult &subTestRes){
+        if (subTestRes.isLeaf)
+        {
+            
+        }
+        
+    }
 };
