fd3288f8d34f87257f5896a62e442479912e3503
zhuhongxuan <4aaaapus@gmail.com>
2024-09-23 10:03:23 +0800

    更新文档

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 - TestDriverClass是负责加载和释放数据，以及调度下层测试类执行测试的对象。类比为一棵树的话，TestDriverClass便是根节点，但TestDriverClass不一定需要位于最顶端，在一些测试量大需要分步读取以免爆内存的情况下，TestDriverClass也可以位于根节点。TestDriverClass构造时需要传入负责加载数据的函数指针以及必要的参数列表。
 - TestContainerClass是负责承接TestDriverClass和TestExecutorClass的节点。类比为树的话，它是树的中间节点。TestContainerClass不执行具体的测试也不负责测试调度，只负责处理中间过程。它会包含数据指针，但既不负责数据初始化也不负责数据清理。
 - TestExecutorClass是具体执行测试的节点，或者类比为树的话是叶子节点。这类节点只负责执行**单个**测试，不应当预期使用它进一步调度其他测试。
+
+在执行测试之前，用户需要：
+1. 定义自定义的子类继承对应需要的父类
+2. 在子类里继承父类中对应的虚函数，并实现对应目标功能
+3. 利用每一个父类中的加载函数将数据加载到需要的节点上，组织成树状结构
--- src/include/TestResult.h
+++ src/include/TestResult.h
@@ -4,15 +4,28 @@ class TestResult {
 public:
     bool success;
     bool isLeaf;
-    u_int32_t currentTestIndex;
-    u_int32_t totalSubTests;
-    std::vector<u_int32_t> failedSubTests;
-    std::vector<std::string> testOutput;
-    void appendSubTestResult(TestResult &subTestRes){
-        if (subTestRes.isLeaf)
+    bool isParentOfLeaf;
+    u_int32_t testIndex;
+    std::vector<TestResult> subTestResults;
+    std::vector<std::string> testVerboseOutput;
+    
+    TestResult(ssize_t subTestCount, bool isParentOfLeaf){
+        this->isParentOfLeaf = isParentOfLeaf;
+        if (isParentOfLeaf)
         {
-            
+            for (ssize_t i = 0; i < subTestCount; i++)
+            {
+                std::string initResult = getStyledText("[?]",TextColor::YELLOW, TextStyle::BOLD);
+                testVerboseOutput.push_back(initResult);
+            }
         }
+    }
+
+    void appendSubTestResult(TestResult subTestRes){
+        subTestResults.push_back(subTestRes);
+    }
+
+    void refrashOutput(ssize_t index, bool succeed){
         
     }
 };
