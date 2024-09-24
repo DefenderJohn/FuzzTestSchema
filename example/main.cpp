7cfd727e1c3d62f616e580436a5499bfbdcde755
DefenderJohn <4aaaapus@gmail.com>
2024-09-24 11:38:50 +0800

    修复CMakeList，编写演示样例

--- example/main.cpp
+++ example/main.cpp
@@ -1,5 +1,72 @@
 #include "FuzzTestSchema.h"
 
+class ExampleTestDriverClass;
+class ExampleTestContainerClass;
+class ExampleTestExecutorClass;
+
+class ExampleTestDriverClass : public TestDriverClass{
+protected:
+    void fillData() {
+        auto matrix = static_cast<std::vector<std::vector<std::string>>*>(this->dataPtr);
+
+        for (int i = 0; i < 5; ++i) {
+            std::vector<std::string> row;
+            for (int j = 0; j < 3; ++j) {
+                row.push_back("String " + std::to_string(i * 3 + j));
+            }
+            matrix->push_back(row);
+        }
+    }
+
+    void SetUp() override {
+        this->dataPtr = new std::vector<std::vector<std::string>>();
+        this->fillData();
+    }
+
+    TestResult RunTest() override {
+        for (size_t i = 0; i < (static_cast<std::vector<std::vector<std::string>>*>(this->dataPtr))->size(); i++)
+        {
+            std::vector<std::string> subData = (static_cast<std::vector<std::vector<std::string>>*>(this->dataPtr))->at(i);
+            ExampleTestContainerClass subClass(true, &subData);
+            TestResult result = subClass.ProceedTest();
+            this->testResult.appendSubTestResult(result);
+        }
+    }
+
+    void TearDown() override {
+        delete static_cast<std::vector<std::vector<std::string>>*>(this->dataPtr);
+    }
+};
+
+class ExampleTestContainerClass : public TestContainerClass{
+public:
+    using TestContainerClass::TestContainerClass;
+    TestResult ProceedTest() override {
+        for (size_t i = 0; i < (static_cast<std::vector<std::string>*>(this->dataPtr))->size(); i++)
+        {
+            std::string subData = (static_cast<std::vector<std::string>*>(this->dataPtr))->at(i);
+            ExampleTestExecutorClass subClass(&subData);
+            TestResult result = subClass.ProceedTest();
+            this->testResult.appendSubTestResult(result);
+            return this->testResult;
+        }
+        
+    }
+};
+
+class ExampleTestExecutorClass : public TestExecutorClass{
+public:
+    using TestExecutorClass::TestExecutorClass;
+    TestResult ProceedTest() override {
+        std::string* data = static_cast<std::string*>(this->dataPtr);
+        // 只是一个演示样例，检查str的长度是否为零
+        int length = data->length();
+        this->testResult.assertNE(length, 0);
+        return this->testResult;
+    }
+};
+
 int main(int argc, char **argv) {
-    
+    ExampleTestDriverClass rootClass;
+    rootClass.ProceedTest();
 }
--- src/include/FuzzTestSchema.h
+++ src/include/FuzzTestSchema.h
@@ -1,6 +1,7 @@
 #ifndef FUZZ_TEST_SCHEMA_H
 #define FUZZ_TEST_SCHEMA_H
 #include <TestResult.h>
+#include <typeinfo>
 class TestDriverClass;
 class TestContainerClass;
 class TestExecutorClass;
@@ -8,27 +9,28 @@ class TestExecutorClass;
 
 class GenericTestClass {
 protected:
-    void* dataPtr;
     TestResult testResult;
-    std::vector<TestDriverClass>    sublevelTestDrivers;
-    std::vector<TestContainerClass> sublevelTestContainers;
-    std::vector<TestExecutorClass>  sublevelTestExecutors;
+    std::vector<GenericTestClass> sublevelTestClasses;
 
 public:
+    void* dataPtr;
     GenericTestClass() {}
     virtual ~GenericTestClass() {}
     virtual TestResult ProceedTest() = 0;
+    void pushTest(GenericTestClass testClass){
+        this->sublevelTestClasses.push_back(testClass);
+    }
 };
 
 class TestDriverClass : public GenericTestClass {
 protected:
     virtual void SetUp() = 0;
     virtual void TearDown() = 0;
-    virtual TestResult runTest() = 0;
+    virtual TestResult RunTest() = 0;
 public:
     TestResult ProceedTest(){
         SetUp();
-        this->testResult.appendSubTestResult(runTest());
+        this->testResult.appendSubTestResult(RunTest());
         TearDown();
         return this->testResult;
     }
@@ -44,14 +46,18 @@ public:
 class TestContainerClass : public GenericTestClass {
 public:
     bool isParentOfLeaf;
-    TestContainerClass(bool isParentOfLeaf){
+    TestContainerClass(bool isParentOfLeaf, void* dataPtr){
         this->isParentOfLeaf = isParentOfLeaf;
+        this->dataPtr = dataPtr;
     }
     virtual TestResult ProceedTest() override;
 };
 
 class TestExecutorClass : public GenericTestClass {
 public:
+    TestExecutorClass(void* dataPtr){
+        this->dataPtr = dataPtr;
+    }
     virtual TestResult ProceedTest() override;
 };
 
